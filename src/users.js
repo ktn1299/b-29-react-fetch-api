046fbb12ac80517003c98a5067f0374016bcb26e
Tamilarasan <ktneditz1299@gmail.com>
2025-01-24 22:05:11 +0530

    Add files via upload

---
+++ src/users.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import './users.css';
+
+const Users = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(data => setUsers(data));
+    }, []);
+
+    return (
+        <div>
+            <h1>User List</h1>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map(user => (
+                        <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Users;
